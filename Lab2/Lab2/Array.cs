5fbb4db8f992014dd84ddf17a7b5358ffa470d5c
MrSklif1337 <sklifden@mail.ru>
2020-10-07 22:40:11 +0300

    Lab2

---
+++ Lab2/Lab2/Array.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab2
+{
+    class Array
+    {
+        private int[] array;
+        private int n;
+        public Array() { }
+
+        public Array(int n)
+        {
+            this.n = n;
+            array = new int[n];
+        }
+
+        public int N
+        {
+            get { return n; }
+            set { if (value > 0) n = 0; }
+        }
+
+        public int this[int i]
+        {
+            get { return array[i]; }
+            set { array[i] = value; }
+        }
+    }
+}
---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое n:");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите первое m:");
+            int m1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе n:");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе m:");
+            int m2 = Convert.ToInt32(Console.ReadLine());
+
+            Matrix mat1 = new Matrix(n1, m1);
+            Matrix mat2 = new Matrix(n2, m2);
+            mat1.FillMatr();
+            mat2.FillMatr();
+
+            Matrix result = MultMatr.StandartMult(mat1, mat2);
+            Console.WriteLine("Первая матрица:");
+            mat1.ReadMatr();
+            Console.WriteLine("Вторая матрица:");
+            mat2.ReadMatr();
+            Console.WriteLine("Результат 1:");
+            result.ReadMatr();
+            Console.WriteLine("Результат 2:");
+            result = MultMatr.VinogradMult(mat1, mat2);
+            result.ReadMatr();
+            Console.WriteLine("Результат 3:");
+            result = MultMatr.VinogradModMult(mat1, mat2);
+            result.ReadMatr();
+        }
+    }
+}
