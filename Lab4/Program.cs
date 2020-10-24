8cc776e42a4ac3039e1aa70e3c6324c4278c0ace
MrSkl1f <sklifden@mail.ru>
2020-10-25 00:08:40 +0300

    Fix

--- Lab4/Program.cs
+++ Lab4/Program.cs
@@ -7,31 +7,7 @@ namespace Lab4
     {
         static void Main(string[] args)
         {
-            Matrix first = new Matrix(600, 600);
-            Matrix second = new Matrix(600, 600);
-            first.FillMatr();
-            second.FillMatr();
-            //first.ReadMatr();
-            Console.WriteLine();
-            //second.ReadMatr();
-            Console.WriteLine();
-
-            Stopwatch clock = new Stopwatch();
-            for (int i = 0; i < 3; i++)
-            {
-                clock.Restart();
-                Matrix res = MultMatr.VinogradMult(first, second);
-                clock.Stop();
-                Console.Write("1 - ");
-                Console.WriteLine(clock.ElapsedTicks);   
-                clock.Restart();
-                res = ParallelMultMatr.ParallelVinogradMult2(first, second, 8);
-                clock.Stop();
-                Console.Write("2 - ");
-                Console.WriteLine(clock.ElapsedTicks);
-                Console.WriteLine();
-            }
-            
+            // NEED TESTS
         }
     }
 }
